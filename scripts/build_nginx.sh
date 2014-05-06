43cbbda2e7308fd286c116c8149733d7811f42ce
Michael Chen <machenmusik@michaelhensmbp2.home>
2014-05-06 10:30:16 -0400

    add example node.js server to be origin; add nginx pushstream module

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -12,11 +12,13 @@
 NGINX_VERSION=1.5.7
 PCRE_VERSION=8.21
 HEADERS_MORE_VERSION=0.23
+PUSHSTREAM_VERSION=0.4.1-dev
 
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
 headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
+pushstream_tarball_url=https://github.com/wandenberg/nginx-push-stream-module/tarball/${HEADERS_MORE_VERSION}.tar.gz
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
 
@@ -30,6 +32,9 @@ echo "Temp dir: $temp_dir"
 echo "Downloading $nginx_tarball_url"
 curl -L $nginx_tarball_url | tar xzv
 
+echo "Downloading $pushstream__tarball_url"
+(cd nginx-${NGINX_VERSION} && curl -L $pushstream_tarball_url | tar xvz )
+
 echo "Downloading $pcre_tarball_url"
 (cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
 
@@ -42,6 +47,7 @@ echo "Downloading $headers_more_nginx_module_url"
 		--with-pcre=pcre-${PCRE_VERSION} \
 		--prefix=/tmp/nginx \
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
+		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/nginx-push-stream-module
 	make install
 )
 
---
+++ server.js
@@ -0,0 +1,72 @@
+/*
+require('log-timestamp')('[%s] %s', function() {
+  return new Date().toJSON();
+});
+*/
+
+var http = require('http');
+var fs = require('fs');
+var env = {};
+
+try { 
+    env = JSON.parse(fs.readFileSync('/home/dotcloud/environment.json', 'utf-8')); 
+    console.log("running on Dotcloud");
+}
+catch (err) {
+    console.log("not running on Dotcloud"); // assume we're not dotcloud. // console.log(err);
+}
+
+var config = { 
+    // external facing service host
+    "host":env['DOTCLOUD_WWW_HTTP_HOST'] || process.env.HOST || "localhost",
+    // actual listening host
+    "listenhost":"0.0.0.0",
+    // external facing service port
+    "port":80,
+    // actual listening port
+    "listenport": env['PORT_WWW'] || process.env.PORT, 
+};
+
+function orelse(value, elsevalue) { return (typeof value === 'undefined') ? elsevalue : value; }
+
+var listenhost = orelse(config.listenhost, config.host);
+var listenport = orelse(config.listenport, config.port);
+
+// okay, now that we've declared the app server, start it up and say so
+
+var server = http.createServer(function(req,res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hi\n');
+});
+
+server.listen(listenport, listenhost);
+console.info("ok, " + config.host + ":" + config.port + " listening on " + listenhost + ":" + listenport); 
+console.info(JSON.stringify(process.env));
+
+
+// this code is to keep the app from hibernating e.g. when running on Heroku
+    
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: config.host,
+            port: config.port,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    //console.log("KeepAlive RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 5 * 60 * 1000); // load every 5 minutes
+}
+
+startKeepAlive();
+
